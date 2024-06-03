faa4513ec2fd34ac0433700a14876affd7975855
deuwin <wcully@gmail.com>
2024-06-03 21:01:58 +0100

    Port across vim options

--- lua/options.lua
+++ lua/options.lua
@@ -3,11 +3,9 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
+-- Make line numbers with relative number default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -41,14 +39,9 @@ vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
+-- Splits appear below and to the right
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
@@ -56,7 +49,109 @@ vim.opt.inccommand = 'split'
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+-- Scroll offset
+-- Lines above/below the cursor when moving vertically
+vim.opt.scrolloff = 7
+-- Characters before/after the cursor when moving horizontally
+vim.opt.wrap = false
+vim.opt.sidescroll = 1
+vim.opt.sidescrolloff = 5
 
--- vim: ts=2 sts=2 sw=2 et
+-- Command line completion
+vim.opt.wildmenu = true
+vim.opt.wildmode = 'longest:full'
+-- Ignore binaries and other artefacts
+vim.opt.wildignore:append { '*.o,*.d,*.so,*.a' }
+vim.opt.wildoptions = 'pum'
+
+-----
+-- Appearance
+--
+
+-- pop-up menu blend
+-- enables pseudo-transparency for the popup-menu
+vim.opt.pumblend = 15
+
+-- show line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- highlight the line the cursor is on
+vim.opt.cursorline = true
+
+-- Rebalance splits on nvim resize
+vim.api.nvim_create_autocmd('VimResized', {
+  callback = function()
+    vim.cmd 'wincmd ='
+  end,
+})
+
+-- show a vertical line at 80 columns
+vim.opt.colorcolumn = '80'
+
+-- display whitespace
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+-- tabs as `   ⇥`
+-- trailing whitespace as `·`
+-- non-breaking space as `·`
+-- indicate line overflow when word wrap is disabled
+vim.opt.listchars = {
+  tab = '  ⇥',
+  trail = '·',
+  nbsp = '·',
+  extends = '>',
+  precedes = '<',
+}
+
+-- disable some of the above for netrw and git commits
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'gitcommit',
+  callback = function()
+    vim.cmd.setlocal('nolist')
+    vim.cmd.setlocal('colorcolumn=')
+  end,
+})
+
+
+------
+-- Formatting
+--
+-- Defaults if not otherwise specified
+vim.opt.textwidth = 80
+vim.opt.formatoptions = 'jcroql'
+-- Tabs
+-- set tab key to insert spaces
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+-- round indent to multiple of 'shiftwidth'.  Applies to > and <
+vim.opt.shiftround = true
+
+
+------
+-- Behaviour
+-- allow backspacing over linebreaks, start of insert, and autoindent
+vim.opt.backspace = { 'eol', 'start', 'indent' }
+-- allow cursor movement keys to move to previous/next line
+vim.opt.whichwrap:append '<,>,h,l'
+
+-- Allow virtual editing (cursor can be positioned anywhere, even when there is
+-- no character) in visual block mode.
+vim.opt.virtualedit = 'block'
+
+
+------
+-- Spelling
+--
+vim.opt.spelllang = 'en_gb'
+-- automatically enable spellcheck in git commits and markdown files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'gitcommit,markdown',
+  callback = function()
+    vim.cmd 'set spell'
+  end,
+})
+
+-- vim: tabstop=2 softtabstop=2 shiftwidth=2 expandtab
