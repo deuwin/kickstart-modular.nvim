516dcd2ebbd00d70c313685b86608d47db29b342
deuwin <wcully@gmail.com>
2024-06-03 21:06:59 +0100

    Add nvim-possession plugin
    https://github.com/gennaro-tedesco/nvim-possession

---
+++ lua/custom/plugins/nvim-possession.lua
@@ -0,0 +1,43 @@
+return {
+    "gennaro-tedesco/nvim-possession",
+    enabled = true,
+    dependencies = {
+        "ibhagwan/fzf-lua",
+    },
+    config = true,
+    init = function()
+        require("nvim-possession").setup({
+            autoload = true,
+            autoswitch = {
+                enable = true,
+            },
+        })
+
+        -- key mappings
+        local possession = require("nvim-possession")
+        vim.keymap.set("n", "<leader>pl",
+            function()
+                possession.list()
+            end,
+            { desc = "[P]ossession,n [L]ist existing sessions" }
+        )
+        vim.keymap.set("n", "<leader>pn",
+            function()
+                possession.new()
+            end,
+            { desc = "[P]ossession, [N]ew session" }
+        )
+        vim.keymap.set("n", "<leader>pu",
+            function()
+                possession.update()
+            end,
+            { desc = "[P]ossession, [U]pdate current session" }
+        )
+        vim.keymap.set("n", "<leader>pd",
+            function()
+                possession.delete()
+            end,
+            { desc = "[P]ossession, [D]elete current session" }
+        )
+    end,
+}
