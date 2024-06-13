0e8cf7c92b47432589384e0a94e07d087e0c29a6
deuwin <wcully@gmail.com>
2024-06-13 19:11:50 +0100

    telescope: Add keymap to open a file in a chosen split

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -29,6 +29,9 @@ return {
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+
+      -- window picker, so telescope can open buffers in chosen split
+      's1n7ax/nvim-window-picker',
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -62,6 +65,26 @@ return {
               -- ['<c-enter>'] = 'to_fuzzy_refine',
               ['jk'] = "close",
               ['kj'] = "close",
+              -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#using-nvim-window-picker-to-choose-a-target-window-when-opening-a-file-from-any-picker
+              ['<C-g>'] = function(prompt_bufnr)
+                -- Use nvim-window-picker to choose the window by dynamically attaching a function
+                local action_set = require('telescope.actions.set')
+                local action_state = require('telescope.actions.state')
+                local picker = action_state.get_current_picker(prompt_bufnr)
+
+                picker.get_selection_window = function(picker, entry)
+                  local picked_window_id = require('window-picker').pick_window({
+                    filter_rules = {
+                      autoselect_one = true,
+                      include_current_win = true,}}
+                  ) or vim.api.nvim_get_current_win()
+                  -- Unbind after using so next instance of the picker acts normally
+                  picker.get_selection_window = nil
+                  return picked_window_id
+                end
+
+                return action_set.edit(prompt_bufnr, 'edit')
+              end,
             },
           },
         },
